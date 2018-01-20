c89829babc444a446bbf95df3e6c149e9fca0429
Unknown <robin.flavigny@free.fr>
2018-01-20 16:33:50 +0100

    MAJ du buid.sh et ajout envoie() et recoie()

--- 24h du code/src/pkg24h/du/code/main.java
+++ 24h du code/src/pkg24h/du/code/main.java
@@ -54,4 +54,28 @@ public class main
     public void getAntInfo(int id){
         
     }
+    
+    public void envoie(String[] cmds) {
+        if(cmds.length != 0) {
+           System.out.println("BEGIN");
+        } 
+        for(int i=0; i<cmds.length; i++) {
+            System.out.println(cmds[i]);
+        }
+        System.out.println("END");     
+    }
+    
+    public String[] recoie() {
+        Scanner sc = new Scanner(System.in);
+        String[] reponse = null;
+        int i = 0;
+        if(sc.nextLine() == "BEGIN") {
+            while(true) {
+                reponse[i] = sc.nextLine();
+                if(reponse[i] == "END") {
+                    return reponse;
+                }
+            }
+        }
+    }
 }
