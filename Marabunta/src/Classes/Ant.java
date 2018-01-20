c3600c3dc8a888253cca7483f8539cbb65d9bdf3
Unknown <raikuz53@gmail.com>
2018-01-20 20:25:08 +0100

    Ant.java mis a jour

--- Marabunta/src/Classes/Ant.java
+++ Marabunta/src/Classes/Ant.java
@@ -52,64 +52,59 @@ public class Ant{
 	public String setMemory(int mem0, int mem1){
 		this.mem0 = mem0;
 		this.mem1 = mem1;
-		return "SET_MEMORY "+mem0+" "+mem1;
+		return "SET_MEMORY "+mem0+" "+mem1+"\n";
 	}
 	
 	//Exclusif
 	public String explore(){
-		--stamina;
-		return "EXPLORE";
+		return checkActionPossible(1)+"EXPLORE"+"\n";
 	}
 	
 	
 	public String putPheromone(int type){
-		stamina -= 3;
-		return "PUT_PHEROMONE "+type;
+		return checkActionPossible(3)+"PUT_PHEROMONE "+type+"\n";
 	}
 	
 	public String refillPheromone(int id){
-		--stamina;
-		return "RECHARGE_PHEROMONE "+id;
+		return checkActionPossible(1)+"RECHARGE_PHEROMONE "+id+"\n";
 	}
 	
 	public String replacePheromone(int id, int type){
-		stamina -= 2;
-		return "CHANGE_PHEROMONE "+id+" "+type;
+		return checkActionPossible(2)+"CHANGE_PHEROMONE "+id+" "+type+"\n";
 	}
 	
 	//Exclusif
 	public String collect(int id,int qtt){
-		stamina -= 4;
-		return "COLLECT "+id+" "+qtt;
+		return checkActionPossible(4)+"COLLECT "+id+" "+qtt+"\n";
 	}
 	
 	//Exclusif
 	public String moveTo(int id){
-		stamina -= 2;
-		return "MOVE_TO "+id;
+		return checkActionPossible(2)+"MOVE_TO "+id+"\n";
 	}
 
 	//Exclusif
 	public String attack(int id, int force){
-		stamina -= force;
-		return "ATTACK "+id+" "+force;
+		return checkActionPossible(force)+"ATTACK "+id+" "+force+"\n";
 	}
 	
 	public String suicide(){
-		return "SUICIDE";
+		return "SUICIDE"+"\n";
 	}
 	
 	public String eat(int qtt){
 		food -= qtt;
 		stamina += qtt * 10;
 		if(stamina > 10000) stamina = 10000;
-		return "EAT "+qtt;
+		return "EAT "+qtt+"\n";
 	}
 	
 	//Exclusif
 	public String exchangeGive(int id, int qtt){
-		food -= qtt;
-		return "DO_TROPHALLAXIS "+id+" "+qtt;
+		if(checkActionPossible(qtt).equals("")) {
+			food -= qtt;
+			return checkActionPossible(qtt)+"DO_TROPHALLAXIS "+id+" "+qtt+"\n";
+		}else return checkActionPossible(qtt);
 	}
 	
 	public void exchangeReceive(int qtt){
@@ -119,13 +114,24 @@ public class Ant{
 	
 	public String enterInTheNest(int id){
 		food = 0;
-		return "NEST "+id;
+		return "NEST "+id+"\n";
 	}
 	
 	//Exclusif
 	public String turn(int angle){
 		if(angle < -180 || angle > 180) angle = 0;
-		--stamina;
-		return "TURN "+angle;
+		return checkActionPossible(1)+"TURN "+angle+"\n";
+	}
+	
+	public String checkActionPossible(int usedStamina) {
+		if(stamina - usedStamina < 1) 
+		{
+			if(food > 0) return eat(1);
+			else return suicide();
+		}
+		else {
+			stamina -= usedStamina;
+			return "";
+		}
 	}
 }
