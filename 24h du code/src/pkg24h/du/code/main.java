d74011906a992dabd38e9fd79b2a656832f48f43
Jules Macaire <julesmacaire1012@live.fr>
2018-01-20 15:25:39 +0100

    Ajout du projet Netbeans (vide)

---
+++ 24h du code/src/pkg24h/du/code/main.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkg24h.du.code;
+
+/**
+ *
+ * @author Zuzu
+ */
+
+import java.util.*;
+
+public class main
+{
+
+    private static int nbrFourmis = 0;
+    private static int food = 200;
+    private static String stream = "";
+    private static int scenarioId = 1;
+    private static ArrayList<Integer> antList = new ArrayList<Integer>();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        while(true){
+            while(stream.length() < 100){
+             
+            }
+        }
+         
+    }
+    
+    public void getNestInfos(){
+        
+    }
+    
+    public void createAnt(){
+       if(nbrFourmis < 12 && food > 100){
+           
+       } 
+    }
+    
+    public void sendAnt(int id){
+        
+    }
+    
+    public void addStream(String partStream){
+        
+    }
+    
+    public void getAntInfo(int id){
+        
+    }
+}
